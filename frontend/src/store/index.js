7989cfeab2527b9c7a6437c6040dcaae72e022ad
Turnsa <daille-@hotmail.fr>
2022-03-28 14:52:26 +0200

    page de login/singup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,119 @@
 import { createStore } from 'vuex'
 
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: "http://localhost:3000/api"
+});
+
+let storage = localStorage.getItem('storage');
+if (storage == null) {
+    storage = {
+      userId: -1,
+      token: '',
+    };
+} else {
+  try {
+    storage = JSON.parse(storage);
+    instance.defaults.headers.common['Authorization'] = 'Bearer' + ' ' + storage.token;
+  } catch (ex) {
+    storage = {
+      userId: -1,
+      token: '',
+    };
+  }
+}
+
 export default createStore({
   state: {
+    // status: '',
+    storage: storage,
+    user: {},
+    // post: {},
+
+    // users: [],
+    // posts: [],
   },
   mutations: {
+    setStatus: function (state, status) {
+      state.status = status;
+    },
+    logUser: function (state, storage) {
+      instance.defaults.headers.common['Authorization'] = 'Bearer' + ' ' + storage.token;
+      localStorage.setItem('storage', JSON.stringify(storage));
+      state.storage = storage;
+    },
+    logout: function (state) {
+      state.storage = {
+        userId: -1,
+        token: '',
+      }
+      localStorage.removeItem('storage');
+    },
+    SET_USER: function (state, user) {
+      state.user = user;
+    },
+    GET_USER_BY_ID(state, user) {
+      state.user = user;
+    },
+    // users: function (state, users) {
+    //   state.users = users;
+    // },
+    // post: function (state, post) {
+    //   state.post = post;
+    // },
+    // posts: function (state, posts) {
+    //   state.posts = posts;
+    // },
   },
   actions: {
+    // users
+    signup: ({commit}, user) => {
+      commit('setStatus', 'loading');
+      return new Promise((resolve, reject) => {
+        instance.post('user/signup', user)
+        .then((response) => {
+          commit('setStatus', 'created');
+          resolve (response);
+        }).catch((error) => {
+          commit('setStatus', 'error_signup');
+          reject (error);
+        })
+      });
+    },
+    login: ({commit}, user) => {
+      commit('setStatus', 'loading');
+      return new Promise((resolve, reject) => {
+        instance.post('user/login', user)
+        .then((response) => {
+          commit('setStatus', '');
+          commit('logUser', response.data);
+          resolve (response);
+        }).catch((error) => {
+          commit('setStatus', 'error_login');
+          reject (error);
+        })
+      });
+    },
+    setUser({ commit }, user) {
+      commit("SET_USER", user);
+    },
+    getUserById({ commit }) {
+      let id = this.state.user.id;
+      console.log(id);
+      instance.get(`user/${id}`)
+      .then((response) => {
+        const user = response.data;
+        commit("GET_USER_BY_ID", user);
+      });
+    },
+    // getUsersInfos: ({commit}) => {
+    //   instance.get('user/')
+    //     .then((response) => {
+    //       commit('users', response.data);
+    //     }).catch(() => {
+    //     })
+    // },
   },
   modules: {
   }
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,201 @@
+<template>
+    <div class="text-center">
+        <div class="headerLogo">
+            <img class="logo" src="../assets/icon-left-font-monochrome-black.svg" alt="logo de groupomania">
+        </div>
+        <div class="card col-4 ">
+            <h1 class="card-title" v-if="mode == 'login' ">Connexion</h1>
+            <h1 class="card-title" v-else>Inscription</h1>
+            <p class="card-subtitle" v-if="mode == 'login' ">Pas encore de compte ? <span class="card__action" @click="switchToSingup()">Créer un compte</span></p>
+            <p class="card-subtitle" v-else>Déjà un compte ? <span class="card__action" @click="switchToLogin()">Se connecter</span></p>
+            <div class="form-row">
+                <input v-model.trim="email" class="form-row__input" type="text" placeholder="Adresse mail"/>
+                <p class="error" v-if="errorEmail == true">Email invalide</p>
+            </div>
+            <div class="form-row" v-if="mode == 'signup'">
+                <input v-model.trim="nom" class="form-row__input" type="text" placeholder="Nom"/>
+                <p class="error" v-if="errorNom == true">Nom invalide</p>
+            </div>
+            <div class="form-row" v-if="mode == 'signup'">
+                <input v-model.trim="prenom" class="form-row__input" type="text" placeholder="Prénom"/>
+                <p class="error" v-if="errorPrenom == true">Prénom invalide</p>
+            </div>
+            <div class="form-row">
+                <input v-model.trim="password" class="form-row__input" type="password" placeholder="Mot de passe"/>
+                <p class="error" v-if="errorPassword == true && mode == 'signup'">Mot de passe invalide (doit comprendre entre 4 et 8 caracteres et au moins 1 chiffre)</p>
+                <p class="error" v-if="errorPassword == true && mode =='login'">Mot de passe invalide</p>
+            </div>
+            <div class="form-row error" v-if="mode == 'login' && status == 'error_login' && this.email != '' && this.password != ''">
+              Adresse mail et/ou mot de passe invalide
+            </div>
+            <div class="form-row error" v-if="mode == 'signup' && status == 'error_signup' && this.email != '' && this.password != ''">
+              Adresse mail déjà utilisée
+            </div>
+
+            <div class="form-row__button">
+                <button @click="login()" class="btn btn-primary" v-if="mode == 'login'">
+                  <span v-if="status == 'loading'">Connexion en cours</span>
+                  <span v-else>Se connecter</span>
+                </button>
+                <button @click="signup()" class="btn btn-primary" v-else>
+                  <span v-if="status == 'loading'">Création en cours</span>
+                  <span v-else>S'inscrire</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import axios from "axios";
+import {mapState} from "vuex";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      mode: "login",
+      email: "",
+      errorEmail: false,
+      nom: "",
+      errorNom: false,
+      prenom: "",
+      errorPrenom: false,
+      password: "",
+      errorPassword: false,
+      errorLogin: "",
+    };
+  },
+    mounted: function() {
+      console.log(this.$store.state.storage);
+      if (this.$store.state.storage.userId != -1) {
+        this.$router.push('/home');
+        return ;
+      }
+    },
+  computed: {
+    ...mapState(['status']),
+  },
+  methods: {
+    switchToSingup() {
+      this.mode = "signup";
+    },
+    switchToLogin() {
+      this.mode = "login";
+    },
+    signup() {
+      const validateNames = new RegExp("^[a-zA-Z ,.'-]+$");
+      const validateEmail = new RegExp(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+      const validatePassword = /^(?=.*\d).{4,8}$/;
+
+      validateEmail.test(this.email)
+        ? (this.errorEmail = false)
+        : (this.errorEmail = true);
+
+      validateNames.test(this.nom)
+        ? (this.errorNom = false)
+        : (this.errorNom = true);
+
+      validateNames.test(this.prenom)
+        ? (this.errorPrenom = false)
+        : (this.errorPrenom = true);
+
+      validatePassword.test(this.password)
+        ? (this.errorPassword = false)
+        : (this.errorPassword = true);
+
+      const user = {
+        nom: this.nom,
+        prenom: this.prenom,
+        email: this.email,
+        password: this.password,
+      };
+      const self = this;
+
+      this.$store.dispatch('signup', user)
+      .then(() => {
+        self.login();
+      }).catch((error) => {
+        console.log(error);
+      })
+    },
+    login() {
+      const user = {
+        email: this.email,
+        password: this.password,
+      };
+      const self = this;
+
+      if (this.email == "") {
+        this.errorEmail = true;
+      }
+      if (this.password == "") {
+        this.errorPassword = true;
+      }
+
+      this.$store.dispatch('login', user)
+      .then((res) => {
+        self.$router.push('/home');
+        console.log(res.data);
+      }).catch((error) => {
+        console.dir(error.response.data.error);
+        this.errorLogin = error.response.data.error;
+      })
+
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p{
+  margin: 0;
+}
+.headerLogo{
+    margin-top: 20px;
+}
+.logo{
+    width: 30%;
+}
+.card__action {
+  color: #2196f3;
+  &:hover{
+    cursor: pointer;
+    text-decoration: underline;
+  }
+}
+.form-row {
+  // display: flex;
+  width: 100%;
+  margin: 8px 0px;
+  gap: 16px;
+  flex-wrap: wrap;
+  &__input {
+    padding: 8px;
+    border: none;
+    border-radius: 8px;
+    background: #f2f2f2;
+    font-weight: 500;
+    font-size: 16px;
+    flex: 1;
+    min-width: 100px;
+    color: black;
+    &::placeholder {
+      color: #aaaaaa;
+    }
+  }
+}
+.card {
+  margin: auto;
+  margin-top: 150px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-radius: 25px;
+  background-color: #fad8d8;
+}
+.error {
+  color: red;
+}
+</style>
