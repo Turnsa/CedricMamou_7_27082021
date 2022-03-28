e0cf92ab4e532193bbe9f14c7db0eca60a39271b
Turnsa <daille-@hotmail.fr>
2022-03-28 14:51:43 +0200

    routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
+import Profile from '@/views/Profile.vue';
+import Setting from '@/views/Setting.vue';
 
 const routes = [
-  {
-    path: '/home',
+  { path: '/home',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/',
+  { path: '/',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: Login
+  },
+  { path: '/user/:id',
+    name: 'Profile',
+    component: Profile
+  },
+  { path: '/setting',
+    name: 'Setting',
+    component: Setting
   }
 ]
 
